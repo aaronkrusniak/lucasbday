083decee82783339ea62ed8eafe427a174ac1cde
somethingaaron <aaron.krusniak@gmail.com>
2017-07-29 11:38:54 -0500

    A test commit to github from PyCharm.

--- testingScript.py
+++ testingScript.py
@@ -5,7 +5,7 @@ import pickle, time
 def main():
     print '\033[0mTEST FONT'
     print 'TEST NUMBER TWO'
+    print 'This has been updated in github.'
     
 if __name__ == '__main__':
     main()
-    
